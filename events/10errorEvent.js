79cd83d3ef6123a916932363b033a4c367967ef7
Veeresh Nerlige <veereshnm@gmail.com>
2015-02-27 09:12:23 -0600

    adding 10errorEvent

--- events/10errorEvent.js
+++ events/10errorEvent.js
@@ -1,5 +1,6 @@
-
 var EventEmitter = require('events').EventEmitter;
 var emitter = new EventEmitter();
 
 emitter.emit('error', new Error('Something terrible happened'));
+
+console.log('this line never executes');
